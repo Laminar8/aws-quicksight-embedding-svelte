6deaa8c03b5dcd62fb97867b52722a2e5d91be1a
CO-Laminar <laminar@comtec.co.kr>
2021-07-05 16:09:52 +0900

    Menu edit

--- Frontend/src/components/common/menu.svelte
+++ Frontend/src/components/common/menu.svelte
@@ -9,25 +9,6 @@
         <a href="#0"><i class="fas fa-angle-down" /><span>메인</span></a>
       </li>
       <li class="menu-item">
-        <a href="#0"><i class="fas fa-angle-down" /><span>사용량</span></a>
-        <ol class="sub-menu">
-          <li class="menu-item"><a href="#0">전체 사용량</a></li>
-          <li class="menu-item">
-            <a href="#/aws/loadbalancer">Zone별 전기사용량</a>
-          </li>
-          <li class="menu-item"><a href="#0">부하별 전기사용량</a></li>
-          <li class="menu-item"><a href="#0">CO2 배출량</a></li>
-          <li class="menu-item"><a href="#0">모터 진단</a></li>
-          <li class="menu-item"><a href="#0">전력 품질</a></li>
-        </ol>
-      </li>
-      <li class="menu-item">
-        <a href="#0"><i class="fas fa-angle-down" /><span>???</span></a>
-      </li>
-      <li class="menu-item">
-        <a href="#0"><i class="fas fa-angle-down" /><span>알람</span></a>
-      </li>
-      <li class="menu-item">
         <a href="#0"><i class="fas fa-angle-down" /><span>설정</span></a>
       </li>
     </ol>
